383c0980f29f34a24c5e7526439046910b4e1c4f
Storm <oguner@gmail.com>
2024-06-04 01:13:53 +0200

    Fix lint issue
    Fix the error:
    /home/runner/work/URLEmbeddedView/URLEmbeddedView/app/src/main/AndroidManifest.xml:
    Error: When targeting Android 13 or higher, posting a permission requires holding
    the POST_NOTIFICATIONS permission
    (usage from com.squareup.picasso.RemoteViewsAction.NotificationAction) [NotificationPermission]

--- app/src/main/java/com/storm/urlembeddedview/MainActivity.java
+++ app/src/main/java/com/storm/urlembeddedview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.storm.urlembeddedview;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +10,11 @@ import com.storm.URLConstants;
 import com.storm.URLEmbeddedData;
 import com.storm.URLEmbeddedView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +22,45 @@ public class MainActivity extends AppCompatActivity {
     private Button btnURL;
     private URLEmbeddedView urlEmbeddedView;
 
+
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // feature requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(
+                this, android.Manifest.permission. POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED) {
+            // You can use the API that requires the permission.
+            //performAction(...);
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this, android.Manifest.permission. POST_NOTIFICATIONS)) {
+            // In an educational UI, explain to the user why your app requires this
+            // permission for a specific feature to behave as expected, and what
+            // features are disabled if it's declined. In this UI, include a
+            // "cancel" or "no thanks" button that lets the user continue
+            // using your app without granting the permission.
+            //showInContextUI(...);
+        } else {
+            // You can directly ask for the permission.
+            // The registered ActivityResultCallback gets the result of this request.
+            requestPermissionLauncher.launch(
+                    android.Manifest.permission. POST_NOTIFICATIONS);
+        }
 
         edtURL = findViewById(R.id.edt_url);
         btnURL = findViewById(R.id.btn_url);
